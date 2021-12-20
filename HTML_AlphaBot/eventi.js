25e6e0ac8cd27ea24200785930bf1754b0d2b23c
Nicholas-Becchis <71812597+Nicholas-Becchis@users.noreply.github.com>
2021-12-20 10:27:21 +0100

    Add files via upload

---
+++ HTML_AlphaBot/eventi.js
@@ -0,0 +1,66 @@
+function su(){ 
+    var comandi = document.getElementById("comandi");
+    comandi.innerHTML='<table id="tblComandi">'+
+    '<tr><td></td><td><input type="image" value="value_su" src="img/freccia_su copia.gif" width="150" height="150" name="action_su" onclick="su();"></td><td></td></tr>'+
+
+    '<tr><td><input type="image" value="value_sx" src="img/freccia_sx.png" width="150" height="150" name="action_sx" onclick="sx();"></td><td><input type="image" value="value_stop" src="img/stop.png" width="150" height="150" name="action_stop" onclick="stop();"></td><td><input type="image" value="value_dx" src="img/freccia_dx.png" width="150" height="150" name="action_dx" onclick="dx();"></td></tr>'+
+
+    '<tr><td></td><td><input type="image" value="value_giu" src="img/freccia_in_giu.png" width="150" height="150" name="action_giu" onclick="giu();"></td><td></td></tr>'+
+    '</table>';
+
+    return;
+}
+
+function sx(){ 
+    var comandi = document.getElementById("comandi");
+    comandi.innerHTML='<table id="tblComandi">'+
+    '<tr><td></td><td><input type="image" value="value_su" src="img/freccia_in_su.png" width="150" height="150" name="action_su" onclick="su();"></td><td></td></tr>'+
+
+    '<tr><td><input type="image" value="value_sx" src="img/freccia_sx copia.gif" width="150" height="150" name="action_sx" onclick="sx();"></td><td><input type="image" value="value_stop" src="img/stop.png" width="150" height="150" name="action_stop" onclick="stop();"></td><td><input type="image" value="value_dx" src="img/freccia_dx.png" width="150" height="150" name="action_dx" onclick="dx();"></td></tr>'+
+
+    '<tr><td></td><td><input type="image" value="value_giu" src="img/freccia_in_giu.png" width="150" height="150" name="action_giu" onclick="giu();"></td><td></td></tr>'+
+    '</table>';
+
+    return;
+}
+
+function dx(){    
+    var comandi = document.getElementById("comandi");
+    comandi.innerHTML='<table id="tblComandi">'+
+    '<tr><td></td><td><input type="image" value="value_su" src="img/freccia_in_su.png" width="150" height="150" name="action_su" onclick="su();"></td><td></td></tr>'+
+
+    '<tr><td><input type="image" value="value_sx" src="img/freccia_sx.png" width="150" height="150" name="action_sx" onclick="sx();"></td><td><input type="image" value="value_stop" src="img/stop.png" width="150" height="150" name="action_stop" onclick="stop();"></td><td><input type="image" value="value_dx" src="img/freccia_dx copia.gif" width="150" height="150" name="action_dx" onclick="dx();"></td></tr>'+
+
+    '<tr><td></td><td><input type="image" value="value_giu" src="img/freccia_in_giu.png" width="150" height="150" name="action_giu" onclick="giu();"></td><td></td></tr>'+
+    '</table>';
+
+    return;
+}
+
+function giu(){  
+    var comandi = document.getElementById("comandi");
+    comandi.innerHTML='<table id="tblComandi">'+
+    '<tr><td></td><td><input type="image" value="value_su" src="img/freccia_in_su.png" width="150" height="150" name="action_su" onclick="su();"></td><td></td></tr>'+
+
+    '<tr><td><input type="image" value="value_sx" src="img/freccia_sx.png" width="150" height="150" name="action_sx" onclick="sx();"></td><td><input type="image" value="value_stop" src="img/stop.png" width="150" height="150" name="action_stop" onclick="stop();"></td><td><input type="image" value="value_dx" src="img/freccia_dx.png" width="150" height="150" name="action_dx" onclick="dx();"></td></tr>'+
+
+    '<tr><td></td><td><input type="image" value="value_giu" src="img/freccia_giu copia.gif" width="150" height="150" name="action_giu" onclick="giu();"></td><td></td></tr>'+
+    '</table>';
+
+    return;
+}
+
+function stop(){    
+    var comandi = document.getElementById("comandi");
+    comandi.innerHTML='<table id="tblComandi">'+
+    '<tr><td></td><td><input type="image" value="value_su" src="img/freccia_in_su.png" width="150" height="150" name="action_su" onclick="su();"></td><td></td></tr>'+
+
+    '<tr><td><input type="image" value="value_sx" src="img/freccia_sx.png" width="150" height="150" name="action_sx" onclick="sx();"></td><td><input type="image" value="value_stop" src="img/stop.png" width="150" height="150" name="action_stop" onclick="stop();"></td><td><input type="image" value="value_dx" src="img/freccia_dx.png" width="150" height="150" name="action_dx" onclick="dx();"></td></tr>'+
+
+    '<tr><td></td><td><input type="image" value="value_giu" src="img/freccia_in_giu.png" width="150" height="150" name="action_giu" onclick="giu();"></td><td></td></tr>'+
+    '</table>';
+
+    return;
+
+    return;
+}
